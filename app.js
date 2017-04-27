5a71787497eec8c9da223fb6f1a454a68fdde05e
rin <neilson.hc.wong@gmail.com>
2017-04-27 17:26:41 -0400

    added ejs stuff and blog template

--- app.js
+++ app.js
@@ -5,16 +5,26 @@ let ms = require('mediaserver');
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
+app.set("view engine", "ejs");
+
 //statics
 app.use(express.static('public'))
 app.use('/mp3', express.static('mp3'));
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + "/html/index.html");
+    res.render("pages/index");
+});
+
+app.get('/about', function(req, res){
+    res.render("pages/about");
 });
 
 app.get('/onsen', function(req, res){
-    res.sendFile(__dirname + "/html/onsen.html");
+    res.render("pages/onsen");
+});
+
+app.get('/blog', function(req, res){
+    res.render("pages/blog");
 });
 
 // app.get(encodeURI('/mp3/01 三日月.mp3'), function(req, res){
@@ -27,4 +37,4 @@ app.get('/onsen', function(req, res){
 //     ms.pipe(req, res, __dirname + "/mp3/02.Over the clouds.mp3");
 // });
 
-app.listen(3000);
+app.listen(4000);
--- public/js/index.js
+++ public/js/index.js
@@ -3,4 +3,11 @@ $(function () {
         $(".viewport").toggleClass("musicStart");
         $(".muse").toggleClass("backstage");
     });
+
+    var link = document.querySelector('link[rel="import"]');
+    var content = link.import;
+    var nav = content.querySelector('.nav');
+
+    $(".viewport").prepend(nav);
+    // document.body.insertBefore(nav, document.body.firstChild);
 });
