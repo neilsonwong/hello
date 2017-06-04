fb66d5b118bb343a0a265c89d70c394c04c72933
Neilson Wong <hc3wong@engmail.uwaterloo.ca>
2017-06-03 20:38:46 -0400

    started adding spa functionality

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,4 @@ $(function () {
         $(".viewport").toggleClass("musicStart");
         $(".muse").toggleClass("backstage");
     });
-
-    var link = document.querySelector('link[rel="import"]');
-    var content = link.import;
-    var nav = content.querySelector('.nav');
-
-    $(".viewport").prepend(nav);
-    // document.body.insertBefore(nav, document.body.firstChild);
 });
