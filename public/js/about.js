f06cf68e96e150d3f6685dfc89a505703edeefaa
Neilson Wong <hc3wong@engmail.uwaterloo.ca>
2018-01-24 00:03:32 -0500

    added maki wip into about page

---
+++ public/js/about.js
@@ -0,0 +1,18 @@
+$(function () {
+	$("body").on("init-about", function(){
+		//init about page vars
+		let maki = getRandomInt(0, 9);
+		let bg = getRandomInt(0, 24);
+
+		console.log(maki);
+		console.log(bg);
+
+		//set css class bgimages
+		$(".about_bg").css("background-image", " url(\"/images/llbg/" + bg + ".png\")");
+		$(".about_chara").css("background-image", " url(\"/images/maki/" + maki + ".png\")");
+	});
+});
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
