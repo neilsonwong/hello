7b047764df72bedc8c90fb2457dfd8642f71e614
rin <neilson.hc.wong@gmail.com>
2018-01-27 17:45:24 -0500

    added arrow scroll snap

--- public/js/about.js
+++ public/js/about.js
@@ -12,6 +12,44 @@ $(function () {
 
 		$(".about_landscape").css("background-image", "linear-gradient( rgba(0,0,0,0.1), rgba(0,0,0,0.25) ), url(\"/images/landscape/" + lbg + ".jpg\")");
 		// $(".about_me").css("background-image", "url(\"/images/me/" + me + ".png\")");
+
+		//override arrow keys
+		var scrollMutex = false;
+        $(document).on({
+			'keydown': function(e) {
+				if (scrollMutex){
+					return;
+				}
+				scrollMutex = true;
+                //make sure this key actually scrolls the page
+                switch (e.which) {
+                    case 34:
+                    case 35:
+                    case 40:
+                        direction = 1;
+                        break;
+                    case 33:
+                    case 36:
+                    case 38:
+                        direction = -1;
+                        break;
+                    default:
+                        //we don't care so lets let it run its natural course
+                        return;
+                }
+
+                e.preventDefault();
+                e.stopPropagation();
+
+				scrollPage(direction);
+            }
+        });
+
+        function scrollPage(direction) {
+            var destination = (direction > 0) ? window.innerHeight : 0;
+			$('.viewport').animate({'scrollTop': destination }, 500, () => { scrollMutex = false; });
+            return;
+        }
 	});
 });
 
