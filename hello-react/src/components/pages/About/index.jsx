21f3ba559342c4aea36e2b5c3eb9647a0ba36e50
Neilson Wong <hc3wong@engmail.uwaterloo.ca>
2018-07-26 11:28:45 -0400

    update about to have roll

--- hello-react/src/components/pages/About/index.jsx
+++ hello-react/src/components/pages/About/index.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
+import FullImage from '../../common/FullImage';
 import Landscape from '../../common/Landscape';
 import SIFBackground from '../../common/SIFBackground';
 
-// import './style.css';
+import './style.css';
 
 export default function About(props) {
   return (
     <div className='about'>
       <Landscape />
       <SIFBackground />
+      <MakiRoll />
     </div>
+  );
+}
+
+function MakiRoll(){
+  return (
+  	<FullImage className='makimono' image='/images/makimono.jpg' />
   );
 }
