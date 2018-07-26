319bb214b87144f5eeaa0b777bca7adcaefedff6
Neilson Wong <hc3wong@engmail.uwaterloo.ca>
2018-07-26 11:05:36 -0400

    add SIFBg

---
+++ hello-react/src/components/common/SIFBackground/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FullImage from '../FullImage';
+
+import './style.css';
+
+export default function SIFBackground(props){
+  let image = '/images/llbg/' + Math.floor(Math.random() * (25)) + '.png';
+
+  return (
+  	<FullImage className='sif-background' image={image} />
+  );
+}
